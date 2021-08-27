bf57d55fb213349413370583100026d777c7a6b1
qiuyuebai <1781313708@qq.com>
2021-08-27 17:28:16 +0800

    完成了 tabBar 的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    
+  </view>
+</template>
+home
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
